e5f3958aa93d4e6509e84b247533e153b0ef0fd8
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2018-10-30 12:28:31 +0100

    Eigenvectors done, but not kmeans

--- mypython.py
+++ mypython.py
@@ -2,12 +2,37 @@
 
 import numpy as np
 import scipy.linalg as la
+from sklearn.cluster import KMeans
+from numpy.linalg import eig
 np.set_printoptions(suppress=True)
 
-A = np.array([[1,3,4],[2,1,3],[4,1,2]])
+A = np.array( [ [0, 1, 1, 0, 0, 0, 0, 0, 0]
+              , [1, 0, 1, 0, 0, 0, 0, 0, 0]
+              , [1, 1, 0, 1, 1, 0, 0, 0, 0]
+              , [0, 0, 1, 0, 1, 1, 1, 0, 0]
+              , [0, 0, 1, 1, 0, 1, 1, 0, 0]
+              , [0, 0, 0, 1, 1, 0, 1, 1, 0]
+              , [0, 0, 0, 1, 1, 1, 0, 1, 0]
+              , [0, 0, 0, 0, 0, 1, 1, 0, 1]
+              , [0, 0, 0, 0, 0, 0, 0, 1, 0]
+              ])
 
-L = np.array([[1,0,0],[2,1,0],[4,11/5,1]])
-U = np.array([[1,3,4],[0,-5,-5],[0,0,-3]])
-print(L.dot(U))
-print(L)
-print(U)
+def printMatrix(M, name):
+  print(name + ":")
+  [print(row) for row in M]
+  print()
+
+
+eigenStuff = eig(A)
+#printMatrix(eigenStuff, "EigenValues and EigenVectors")
+
+secondSmallestEigenVector = eigenStuff[1][1]
+print(secondSmallestEigenVector)
+
+kmeans = KMeans(n_clusters=2, init=secondSmallestEigenVector)
+#kmeans.cluster_centers_
+
+
+print(kmeans)
+
+
