f29a118911b3bb67dd34c821e74759571dd60b36
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2019-01-13 15:13:06 +0100

    Add simple example from slides

--- clustering.py
+++ clustering.py
@@ -18,12 +18,20 @@ A = np.array( [ [0, 1, 1, 0, 0, 0, 0, 0, 0]
               , [0, 0, 0, 0, 0, 0, 0, 1, 0]
               ])
 
+Asmall = np.array( [ [0, 1, 1, 0, 0, 0]
+              , [1, 0, 1, 0, 0, 0]
+              , [1, 1, 0, 1, 0, 0]
+              , [0, 0, 1, 0, 1, 1]
+              , [0, 0, 0, 1, 0, 1]
+              , [0, 0, 0, 1, 1, 0]
+              ])
+
 def mkLaplacian(A):
-  D = [sum(row) for row in A]# * np.eye(A.shape[0])
-  A = A * -1
+  L = A * -1
+  D = [sum(row) for row in A]
   for x in range(0, len(D)):
-    A[x,x] = D[x] + A[x,x]
-  return A
+    L[x,x] = D[x] + A[x,x]
+  return L
 
 def mkEigenDecom(L):
   return npl.eig(L)
@@ -59,28 +67,44 @@ def getClusters(amount, eigenVector):
 
   return kmeans.fit(transposed.real).labels_
 
-print("Loading network...")
-A, _ = fileLoad.getSparseFriendsDefault()
-print("Done")
-print("Laplacian... ")
-L = mkLaplacian(A)
-print("Done")
-print("Getting eigendecomposition...")
-eiDecom = mkEigenDecom(L)
-print("Done")
-print("Getting second smallest eigen vector...")
-sSEV = getSecondSmallestEigenVector(eiDecom)
-print("Done")
-print("\"Computing\" cluster amounts...")
-clustAmount = getClusterAmount(sSEV)
-print("Done")
-print("Getting clusters...")
-clusters = getClusters(clustAmount, sSEV)
-print(clusters)
+def runSmallExample(): # from Lecture 7, slide 28
+  L = mkLaplacian(Asmall)
+  print(L)
+  eiDecom = mkEigenDecom(L)
+  print(eiDecom)
+  sSEV = getSecondSmallestEigenVector(eiDecom)
+  print(sSEV)
+  clusters = getClusters(2, sSEV)  
+  print(clusters)
 
-print("Writing clusters to file: 'material/clusteringResults.txt'...")
-with open('material/clusteringResults.txt', 'w') as f:
-  for item in clusters:
-    f.write("%s\n" % item)
+def run():
+  print("Loading network...")
+  A, _ = fileLoad.getSparseFriendsDefault()
+  print("Done")
+  print("Laplacian... ")
+  L = mkLaplacian(A)
+  print("Done")
+  print("Getting eigendecomposition...")
+  eiDecom = mkEigenDecom(L)
+  print("Done")
+  print("Getting second smallest eigen vector...")
+  sSEV = getSecondSmallestEigenVector(eiDecom)
+  print("Done")
+  print("\"Computing\" cluster amounts...")
+  clustAmount = getClusterAmount(sSEV)
+  print("Done")
+  print("Getting clusters...")
+  clusters = getClusters(clustAmount, sSEV)
+  print(clusters)
 
-print("Done")
+  print("Writing clusters to file: 'material/clusteringResults.txt'...")
+  with open('material/clusteringResults.txt', 'w') as f:
+    for item in clusters:
+      f.write("%s\n" % item)
+
+  print("Done")
+
+
+
+#runSmallExample()
+run()
